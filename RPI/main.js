c99260bf993cefa84be338f4a4a776efa31913ba
Wildan Zulfikar <hey@wzulfikar.com>
2022-11-18 15:40:19 +0200

    Use MAC address for sensor id

--- RPI/main.js
+++ RPI/main.js
@@ -1,16 +1,17 @@
 const monitor = require("./monitor");
 const db = require("./db");
+const getSensorId = require("./lib/getSensorId")
 
 const table = "csproject_co2_reading";
 
 (async () => {
   const { store } = await db.init();
   const session_id = createSessionId();
+  const sensor_id = await getSensorId();
 
   monitor.start(async (value) => {
     // console.log("ppm:", value);
-
-    const row = await store(table, { session_id, value });
+    const row = await store(table, { sensor_id, session_id, value });
     console.clear();
     console.log(`reading stored: ${row.value} ppm`);
   });
